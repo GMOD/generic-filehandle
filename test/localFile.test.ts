8c3c00dac6cb3eab3402b218d9d7a0d2113165f4
Colin <colin.diesh@gmail.com>
2019-04-09 21:13:57 -0400

    Test local file handle

--- test/localFile.test.ts
+++ test/localFile.test.ts
@@ -6,4 +6,18 @@ describe('local file tests', () => {
     const b = await f.readFile()
     expect(b.toString()).toEqual('testing\n')
   })
+  it('reads local file', async () => {
+    const f = new LocalFile(require.resolve('./data/test.txt'))
+    const buf = Buffer.allocUnsafe(3)
+    const bytesRead = await f.read(buf, 0, 3, 0)
+    expect(buf.toString()).toEqual('tes')
+    expect(bytesRead).toEqual(3)
+  })
+  it('reads local file clipped at the end', async () => {
+    const f = new LocalFile(require.resolve('./data/test.txt'))
+    const buf = Buffer.allocUnsafe(3)
+    const bytesRead = await f.read(buf, 0, 3, 6)
+    expect(buf.slice(0,bytesRead).toString()).toEqual('g\n')
+    expect(bytesRead).toEqual(2)
+  })
 })
