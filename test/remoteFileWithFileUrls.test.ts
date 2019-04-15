b736a2d56822cbcb1b784717c3d249b89d129e65
Robert Buels <rbuels@gmail.com>
2019-04-15 13:55:26 -0700

    encoding error wording varies

--- test/remoteFileWithFileUrls.test.ts
+++ test/remoteFileWithFileUrls.test.ts
@@ -15,7 +15,7 @@ describe("remote file with file urls", () => {
     expect(fileText).toEqual("testing\n");
     const fileText2 = await f.readFile({ encoding: "utf8" });
     expect(fileText2).toEqual("testing\n");
-    await expect(f.readFile("fakeEncoding")).rejects.toThrow(/is invalid/);
+    await expect(f.readFile("fakeEncoding")).rejects.toThrow(/encoding/);
   });
   it("reads remote partially", async () => {
     const f = new RemoteFile(`${fileUrlBase}/test.txt`);
