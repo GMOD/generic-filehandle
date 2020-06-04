97b11586f1c6e55f1ed0d76d28de2303a7a31cdc
Colin <colin.diesh@gmail.com>
2020-06-04 18:57:12 -0400

    Add test for passing authorization token through remoteFile

--- test/remoteFile.test.ts
+++ test/remoteFile.test.ts
@@ -175,4 +175,42 @@ describe('remote file tests', () => {
     const stat = await f.stat()
     expect(stat.size).toEqual(8)
   })
+  it('auth token', async () => {
+    fetchMock.mock('http://fakehost/test.txt', (url: string, args: any) => {
+      if (args.headers.Authorization) {
+        return {
+          status: 200,
+          body: 'hello world',
+        }
+      } else {
+        return { status: 403 }
+      }
+    })
+    const f = new RemoteFile('http://fakehost/test.txt', {
+      overrides: { headers: { Authorization: 'Basic YWxhZGRpbjpvcGVuc2VzYW1l' } },
+    })
+    const stat = await f.readFile('utf8')
+    expect(stat).toBe('hello world')
+  })
+  it('auth token with range request', async () => {
+    fetchMock.mock('http://fakehost/test.txt', (url: string, args: any) => {
+      console.log(args.headers)
+      if (args.headers.Authorization && args.headers.range) {
+        return {
+          status: 206,
+          body: 'hello world',
+        }
+      } else if (!args.headers.Authorization) {
+        return { status: 403 }
+      } else if (!args.headers.Range) {
+        return { status: 400 }
+      }
+    })
+    const f = new RemoteFile('http://fakehost/test.txt', {
+      overrides: { headers: { Authorization: 'Basic YWxhZGRpbjpvcGVuc2VzYW1l' } },
+    })
+    const { buffer } = await f.read(Buffer.alloc(5), 0, 5, 0)
+    const str = buffer.toString()
+    expect(str).toBe('hello')
+  })
 })
